82cd7867b05b79940d8c35c3ea42467d973f227b
Bruno Pinto <bpinto@thoughtworks.com>
2012-07-24 21:16:18 -0300

    user functions have the highest priority

--- oh-my-fish.fish
+++ oh-my-fish.fish
@@ -1,3 +1,7 @@
+# Extracting user's functions – will be added later.
+set user_function_path $fish_function_path[1]
+set -e fish_function_path[1]
+
 # Add all functions to fish_funtion_path.
 set fish_function_path $FISH/functions/ $fish_function_path
 
@@ -6,5 +10,8 @@ for plugin in $FISH_PLUGINS
   set fish_function_path $FISH/plugins/$plugin $fish_function_path
 end
 
-# Add theme to fish_function_path.
+# Add user defined theme to fish_function_path.
 set fish_function_path /Users/bpinto/.oh-my-fish/themes/$FISH_THEME $fish_function_path
+
+# Re-adding user's functions so they have the highest priority
+set fish_function_path $user_function_path $fish_function_path
