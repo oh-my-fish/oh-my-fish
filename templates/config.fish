2c4b02710dd63176e379167fc68e7fb7324eafe8
Bruno Pinto <bpinto@thoughtworks.com>
2012-07-26 01:03:51 -0300

    template configuration file

---
+++ templates/config.fish
@@ -0,0 +1,15 @@
+# Path to your oh-my-fish.
+set FISH $HOME/.oh-my-fish
+
+# Theme
+set FISH_THEME robbyrussell
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set FISH_PLUGINS autojump git
+
+# Path to your custom folder (default path is $FISH/custom)
+#set FISH_CUSTOM $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish cofiguration.
+. $FISH/oh-my-fish.fish
