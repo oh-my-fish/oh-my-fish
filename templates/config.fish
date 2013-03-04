01c60a0507440c4e4ef3c6b0436988381a84bf93
Kris Leech <kris.leech@gmail.com>
2013-03-04 09:06:35 +0000

    Replace non-existent `git` plugin with `bundler` in template used to generate config.fish

--- templates/config.fish
+++ templates/config.fish
@@ -6,7 +6,7 @@ set fish_theme robbyrussell
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump git
+# Example format: set fish_plugins autojump bundler
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
