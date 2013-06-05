0b92a704e10d7e31d7db654b9c5108df68c390c1
Jason Rogers <jason.rogers@samasource.org>
2013-06-05 12:42:51 -0400

    adding my Git-based theme

---
+++ themes/jacaetevha/fish_prompt.fish
@@ -0,0 +1,27 @@
+function fish_prompt
+   set_color yellow
+   printf '%s' (whoami)
+   set_color normal
+   printf ' at '
+
+   set_color magenta
+   printf '%s' (hostname|cut -d . -f 1)
+   set_color normal
+   printf ' in '
+
+   set_color $fish_color_cwd
+   printf '%s ' (prompt_pwd)
+   set_color blue
+   printf '%s' (/Users/jrogers/bin/TBytes.sh)
+   set_color normal
+   printf '%s' (__fish_git_prompt)
+
+   # Line 2
+   echo
+   if test $VIRTUAL_ENV
+       printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+   end
+   printf '↪  '
+   set_color normal
+end
+
---
+++ themes/jacaetevha/fish_right_prompt.fish
@@ -0,0 +1,6 @@
+function fish_right_prompt -d "Write out the right prompt"
+    set_color yellow
+    # date "+%m/%d/%y"
+    date
+    set_color normal
+end
