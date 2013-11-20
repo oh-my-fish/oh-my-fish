d140552b1259a655424b9b72edaf25cf88806d81
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-11-20 07:04:45 -0200

    removing copy prompts function as it does not setup the theme using the environment variable

--- copy_prompts.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/fish
-# This script will create a symbolic link between all themes included in oh-my-fish to the sample prompts
-# This will allow the user to choose an oh-my-fish theme while using the stock fish_config web application
-for themePath in (ls $fish_path/themes/*/fish_prompt.fish)
-    set themeName (echo $themePath | sed "s/.*themes\///;s/\/fish_prompt.fish//")
-    echo "Adding $themeName to the sample prompts"
-    ln -sf $themePath $__fish_datadir/tools/web_config/sample_prompts/$themeName.fish
-end
