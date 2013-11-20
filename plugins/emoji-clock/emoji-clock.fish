00236e5af369631a8d5aff64a4bdaaa82efd54ed
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-11-20 07:53:39 -0200

    tidying

--- plugins/emoji-clock/emoji-clock.fish
+++ plugins/emoji-clock/emoji-clock.fish
@@ -12,77 +12,77 @@ function emoji-clock
   set minutes (date '+%M')
   switch $hour
     case 01
-		if test $minutes -ge 30 
-			set clock "🕜"
-		else
-			set clock "🕐" 
-		end
+    if test $minutes -ge 30
+      set clock "🕜"
+    else
+      set clock "🕐"
+    end
     case 02
-		if test $minutes -ge 30 
-			set clock "🕝"
-		else
-			set clock "🕑" 
-		end
+    if test $minutes -ge 30
+      set clock "🕝"
+    else
+      set clock "🕑"
+    end
     case 03
-		if test $minutes -ge 30 
-			set clock "🕞"
-		else
-			set clock "🕒" 
-		end
+    if test $minutes -ge 30
+      set clock "🕞"
+    else
+      set clock "🕒"
+    end
     case 04
-		if test $minutes -ge 30 
-			set clock "🕟"
-		else
-			set clock "🕓" 
-		end
+    if test $minutes -ge 30
+      set clock "🕟"
+    else
+      set clock "🕓"
+    end
     case 05
-		if test $minutes -ge 30 
-			set clock "🕠"
-		else
-			set clock "🕔" 
-		end
+    if test $minutes -ge 30
+      set clock "🕠"
+    else
+      set clock "🕔"
+    end
     case 06
-		if test $minutes -ge 30 
-			set clock "🕡"
-		else
-			set clock "🕕" 
-		end
+    if test $minutes -ge 30
+      set clock "🕡"
+    else
+      set clock "🕕"
+    end
     case 07
-		if test $minutes -ge 30 
-			set clock "🕢"
-		else
-			set clock "🕖" 
-		end
+    if test $minutes -ge 30
+      set clock "🕢"
+    else
+      set clock "🕖"
+    end
     case 08
-		if test $minutes -ge 30 
-			set clock "🕣"
-		else
-			set clock "🕗" 
-		end
+    if test $minutes -ge 30
+      set clock "🕣"
+    else
+      set clock "🕗"
+    end
     case 09
-		if test $minutes -ge 30 
-			set clock "🕤"
-		else
-			set clock "🕘" 
-		end
+    if test $minutes -ge 30
+      set clock "🕤"
+    else
+      set clock "🕘"
+    end
     case 10
-		if test $minutes -ge 30 
-			set clock "🕥"
-		else
-			set clock "🕙" 
-		end
+    if test $minutes -ge 30
+      set clock "🕥"
+    else
+      set clock "🕙"
+    end
     case 11
-		if test $minutes -ge 30 
-			set clock "🕦"
-		else
-			set clock "🕚" 
-		end
+    if test $minutes -ge 30
+      set clock "🕦"
+    else
+      set clock "🕚"
+    end
     case 12
-		if test $minutes -ge 30 
-			set clock "🕧"
-		else
-			set clock "🕛" 
-		end
+    if test $minutes -ge 30
+      set clock "🕧"
+    else
+      set clock "🕛"
+    end
     case '*' set clock "⌛"
   end
   echo $clock
