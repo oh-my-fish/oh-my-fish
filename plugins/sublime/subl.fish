6986fb1c2db3055f24b4d8f0a251d4f692171229
Joshua Warner <joshua.warner@readytalk.com>
2013-12-11 15:59:32 -0700

    Implement subl for Linux (assuming a standard .deb install of v3)

--- plugins/sublime/subl.fish
+++ plugins/sublime/subl.fish
@@ -3,6 +3,8 @@ function subl --description 'Open Sublime Text'
     "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" $argv
   else if test -d "/Applications/Sublime Text 2.app"
     "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" $argv
+  else if test -x "/opt/sublime_text/sublime_text"
+    "/opt/sublime_text/sublime_text" $argv
   else
     echo "No Sublime Text installation found"
   end
