0622fe860fec06c0e9a36a154f1025280671e495
Steven Knight <steven@knight.cx>
2014-02-26 17:39:28 -0500

    Ported function to zsh

--- plugins/archlinux/pacman/pacmansignkeys.fish
+++ plugins/archlinux/pacman/pacmansignkeys.fish
@@ -1,9 +1,8 @@
-# TODO: Need to convert to fish
-function pacmansignkeys
-  # for key in $*; do
-  #   sudo pacman-key --recv-keys $key
-  #   sudo pacman-key --lsign-key $key
-  #   printf 'trust\n3\n' | sudo gpg --homedir /etc/pacman.d/gnupg \
-  #     --no-permission-warning --command-fd 0 --edit-key $key
-  # done
+function pacmansignkeys -d "Allow user to sign keys"
+  for key in $argv;
+    sudo pacman-key --recv-keys $key
+    sudo pacman-key --lsign-key $key
+    printf 'trust\n3\n' | sudo gpg --homedir /etc/pacman.d/gnupg \
+        --no-permission-warning --command-fd 0 --edit-key $key
+  end
 end
