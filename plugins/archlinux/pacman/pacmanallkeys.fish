1df49b6fcdfa1e8f9b9f5478a8ca19fcab3473f4
Steven Knight <steven@knight.cx>
2014-02-26 17:39:15 -0500

    Ported function to zsh

--- plugins/archlinux/pacman/pacmanallkeys.fish
+++ plugins/archlinux/pacman/pacmanallkeys.fish
@@ -1,7 +1,3 @@
-# TODO: Need to convert to fish
-function pacmanallkeys
-# Get all keys for developers and trusted users
-#curl https://www.archlinux.org/{developers,trustedusers}/ |
-#awk -F\" '(/pgp.mit.edu/) {sub(/.*search=0x/,"");print $1}' |
-#xargs sudo pacman-key --recv-keys
+function pacmanallkeys -d "Get all keys for developers and trusted users"
+    curl https://www.archlinux.org/developers/ https://www.archlinux.org/trustedusers/ | awk -F\" '(/pgp.mit.edu/) {sub(/.*search=0x/,"");print $1}' | xargs sudo pacman-key --recv-keys
 end
