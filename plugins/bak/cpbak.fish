33b5d64e7f76244ba7b8544abd9ff3233f4fa1f9
Roman Inflianskas <infroma@gmail.com>
2014-06-11 18:55:53 +0400

    bak plugin: bak renamed to mvbak, cpbak added

---
+++ plugins/bak/cpbak.fish
@@ -0,0 +1,6 @@
+# Function to copy files and directories (a.txt copy to a.txt.20140608_195859.bak)
+# (c) Roman Inflianskas (rominf) <infroma@gmail.com>, 2014
+
+function cpbak
+  __bak cp $argv
+end
---
+++ plugins/bak/mvbak.fish
@@ -0,0 +1,6 @@
+# Function to rename files and directories (a.txt rename to a.txt.20140608_195859.bak)
+# (c) Roman Inflianskas (rominf) <infroma@gmail.com>, 2014
+
+function mvbak
+  __bak mv $argv
+end
