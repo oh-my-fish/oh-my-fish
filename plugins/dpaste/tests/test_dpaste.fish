668849a9c3b052ead6759f2b5d0f266e4653a21e
Roman Inflianskas <infroma@gmail.com>
2014-06-11 23:50:29 +0400

    [dpaste] Fix tests

--- plugins/dpaste/tests/test_dpaste.fish
+++ plugins/dpaste/tests/test_dpaste.fish
@@ -10,10 +10,13 @@ function suite_dpaste
   function test_dpaste_parse_expires
     assert_equal text (__dpaste_parse_expires text)
     assert_equal "https://dpaste.de/api/?format=url" $__dpaste_send_url
+    set -g __dpaste_send_url $dpaste_url
     assert_equal text (__dpaste_parse_expires -t never text)
     assert_equal "https://dpaste.de/api/?format=url&expires=never" $__dpaste_send_url
-    assert_equal text (__dpaste_parse_expires -t hour 3600)
-    assert_equal "https://dpaste.de/api/?format=url&expires=" $__dpaste_send_url
+    set -g __dpaste_send_url $dpaste_url
+    assert_equal text (__dpaste_parse_expires -t hour text)
+    assert_equal "https://dpaste.de/api/?format=url&expires=3600" $__dpaste_send_url
+    set -g __dpaste_send_url $dpaste_url
   end
 end
 
