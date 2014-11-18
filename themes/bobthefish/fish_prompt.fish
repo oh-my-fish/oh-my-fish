f046c9c5422a83f0fa72c4adc84aed52d14fb624
Justin Hileman <justin@justinhileman.info>
2014-11-17 18:40:30 -0800

    [bobthefish] Add ☿ (mercury) glyph to hg prompt
    This makes it easier (possible?) to distinguish between hg and git repos at a glance.

--- themes/bobthefish/fish_prompt.fish
+++ themes/bobthefish/fish_prompt.fish
@@ -31,6 +31,7 @@ set __bobthefish_detached_glyph          \u27A6
 set __bobthefish_nonzero_exit_glyph      '! '
 set __bobthefish_superuser_glyph         '$ '
 set __bobthefish_bg_job_glyph            '% '
+set __bobthefish_hg_glyph                \u263F
 
 # Python glyphs
 set __bobthefish_superscript_glyph       \u00B9 \u00B2 \u00B3
@@ -248,6 +249,9 @@ function __bobthefish_prompt_hg -d 'Display the actual hg state'
   end
 
   __bobthefish_path_segment (__bobthefish_project_dir_hg)
+
+  __bobthefish_start_segment $flag_bg $flag_fg
+  echo -n -s $__bobthefish_hg_glyph ' '
 
   __bobthefish_start_segment $flag_bg $flag_fg
   set_color $flag_fg --bold
