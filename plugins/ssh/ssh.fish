c761f9442ce9b96b283e36600579250cfe41c906
Wild Kat <wk@futureinquestion.net>
2014-11-29 23:10:55 +0100

    Added documentation link for ssh helper plugin and further cleanup

--- plugins/ssh/ssh.fish
+++ plugins/ssh/ssh.fish
@@ -1,12 +1,12 @@
 function ssh --description 'OpenSSH SSH client (remote login program) with a conservative $TERM value'
   switch $TERM
-    case screen-256color
-        set -l -x TERM screen
+    case screen-256color 
+        set -lx TERM screen
         command ssh $argv
     case xterm-256color
-        set -l -x TERM xterm
+        set -lx TERM xterm
         command ssh $argv
-    case '*'
+    case '*' 
       command ssh $argv
     end
 end
