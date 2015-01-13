e7afb310a42d2cbd444543c947f7b2097f325c99
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-01-13 13:36:27 -0200

    removing bucaran fork

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -3,4 +3,4 @@ ppa=ppa:fish-shell/${FISH_PPA:-nightly-master}
 sudo add-apt-repository -y $ppa
 sudo apt-get update
 sudo apt-get -y install fish
-curl -L https://github.com/bpinto/oh-my-fish/raw/master/tools/install.fish | FORK=bucaran fish
+curl -L https://github.com/bpinto/oh-my-fish/raw/master/tools/install.fish | fish
