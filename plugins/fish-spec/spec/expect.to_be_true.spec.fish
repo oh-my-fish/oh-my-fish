cb3ce0317afb470bc8b74b319b22944fc5da7ae8
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-01-14 22:54:43 -0200

    expect --to-be-true tests

---
+++ plugins/fish-spec/spec/expect.to_be_true.spec.fish
@@ -0,0 +1,29 @@
+import plugins/fish-spec
+
+function describe_to_be_true -d 'expect --to-be-true'
+  function before_each
+    set -e result
+  end
+
+  function it_returns_0_when_evaluated_value_returns_0
+    echo (expect true --to-be-true; set result $status) >/dev/null
+    expect $result --to-equal 0
+  end
+
+  function it_returns_1_when_evaluated_value_returns_1
+    echo (expect false --to-be-true; set result $status) >/dev/null
+    expect $result --to-equal 1
+  end
+
+  function it_returns_0_when_evaluated_function_returns_0
+    echo (expect "eval true" --to-be-true; set result $status) >/dev/null
+    expect $result --to-equal 0
+  end
+
+  function it_returns_1_when_evaluated_function_returns_1
+    echo (expect "eval false" --to-be-true; set result $status) >/dev/null
+    expect $result --to-equal 1
+  end
+end
+
+spec.run $argv
