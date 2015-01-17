b1fee8b857ea090ffd3c7534390f55aee05f445c
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-01-17 01:21:03 -0200

    running tests on linux and osx

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-ppa=ppa:fish-shell/${FISH_PPA:-nightly-master}
-sudo add-apt-repository -y $ppa
-sudo apt-get update
-sudo apt-get -y install fish
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  ppa=ppa:fish-shell/$FISH_PPA
+  sudo add-apt-repository -y $ppa
+  sudo apt-get update
+  sudo apt-get -y install fish
+else
+  brew install fish $BREW_OPTIONS
+fi
+
 curl -L https://github.com/$TRAVIS_REPO_SLUG/raw/$TRAVIS_BRANCH/tools/install.fish | fish
