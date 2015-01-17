0bfb95a504ec071b9e0671886a66bf2dd3bde1f8
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-01-17 03:12:44 -0200

    run tests from correct branch on travis

--- script/run-tests.fish
+++ script/run-tests.fish
@@ -1,4 +1,10 @@
 #!/usr/bin/env fish
+
+# When running on CI, run tests from the correct branch
+if set -q TRAVIS_BRANCH
+  command git checkout $TRAVIS_BRANCH
+end
+
 set -l result 0
 for test in (find * -type f -print | grep "spec.fish")
   fish $test $argv
