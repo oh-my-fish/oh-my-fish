344fbb4f36c259d41eaea598362cfb6019c92f6f
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-01-17 01:05:54 -0200

    run tests from correct repo on travis

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -3,4 +3,4 @@ ppa=ppa:fish-shell/${FISH_PPA:-nightly-master}
 sudo add-apt-repository -y $ppa
 sudo apt-get update
 sudo apt-get -y install fish
-curl -L https://github.com/bpinto/oh-my-fish/raw/master/tools/install.fish | fish
+curl -L https://github.com/$TRAVIS_REPO_SLUG/raw/$TRAVIS_BRANCH/tools/install.fish | fish
