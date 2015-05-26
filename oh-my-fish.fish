25ef546fc73f9c509e30b8c18b25ab5c10db5230
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-05-26 14:10:58 +0100

    simplification: do not prioritize '.config/fish/functions'

--- oh-my-fish.fish
+++ oh-my-fish.fish
@@ -4,17 +4,10 @@ if not set -q fish_custom
   set -g fish_custom $fish_path/custom
 end
 
-# Extract user defined functions from path and prepend later to
-# avoid collisions with oh-my-fish internal functions and allow
-# users to override/customize plugins, themes, etc.
-set user_function_path $fish_function_path[1]
-set -e fish_function_path[1]
-
 # Add functions defined in oh-my-fish/functions to the path.
 if not contains $fish_path/functions/ $fish_function_path
   set fish_function_path $fish_path/functions/ $fish_function_path
 end
-
 
 # Add imported plugins, completions and themes. Customize imported
 # commands via the $fish_path/custom directory, for example create
@@ -27,9 +20,6 @@ import plugins/$fish_plugins themes/$fish_theme
 for load in $fish_custom/*.load
   . $load
 end
-
-# Prepend extracted user functions so they have the highest priority.
-set fish_function_path $user_function_path $fish_function_path
 
 # Make sure to exit with $status of 1 when reloading the framework.
 or true
