b2549c2005b4d1534a0c4041cf16381b075c5c2b
Justin Hileman <justin@justinhileman.info>
2015-05-31 08:30:48 -0700

    [split] Move bak plugin to oh-my-fish/plugin-bak
    https://github.com/oh-my-fish/plugin-bak

--- plugins/bak/mvbak.fish
+++
@@ -1,6 +0,0 @@
-# Function to rename files and directories (a.txt rename to a.txt.20140608_195859.bak)
-# (c) Roman Inflianskas (rominf) <infroma@gmail.com>, 2014
-
-function mvbak --description 'Rename files to make a backup copies'
-  __bak mvbak mv $argv
-end
--- plugins/bak/unmvbak.fish
+++
@@ -1,6 +0,0 @@
-# Function to move files and directories (a.txt.20140608_195859.bak move to a.txt)
-# (c) Roman Inflianskas (rominf) <infroma@gmail.com>, 2014
-
-function unmvbak --description 'Move files to revert a backup copies to a normal files'
-  __unbak unmvbak mv $argv
-end
