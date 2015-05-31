3cb0cc4334fdb5258752c7beab36934eb77fa8b3
Justin Hileman <justin@justinhileman.info>
2015-05-31 08:30:49 -0700

    [split] Move pbcopy plugin to oh-my-fish/plugin-pbcopy
    https://github.com/oh-my-fish/plugin-pbcopy

--- plugins/pbcopy/pbcopy.fish
+++
@@ -1,7 +0,0 @@
-function pbcopy --description "Copy data from STDIN to the clipboard"
-  xsel --clipboard --input
-end
-
-function pbpaste  --description "Paste data from the Clipboard"
-  xsel --clipboard --output
-end
