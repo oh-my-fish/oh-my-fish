497350c771434e0222f97ea7f2b6bdbaa4db370c
Justin Hileman <justin@justinhileman.info>
2015-05-31 08:30:49 -0700

    [split] Move python plugin to oh-my-fish/plugin-python
    https://github.com/oh-my-fish/plugin-python

--- plugins/python/_python.fish
+++
@@ -1,8 +0,0 @@
-# Use python2 if found, otherwise fallback to python.
-function _python
-  if which python2
-    python2 $argv
-  else
-    python $argv
-  end
-end
--- plugins/python/pybeautifyjson.fish
+++
@@ -1,5 +0,0 @@
-# beautify json string
-# use : pybeautifyjson '{"foo": "lorem", "bar": "ipsum"}'
-function pybeautifyjson
-  echo $argv | _python -mjson.tool
-end
