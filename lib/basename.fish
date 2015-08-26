bf52f5d57a8598f2e0c67fe20c5bf3359618f5ae
Jorge Bucaran <jbucaran@me.com>
2015-08-27 03:55:16 +0900

    fix basename polyfill

--- lib/basename.fish
+++ lib/basename.fish
@@ -3,27 +3,23 @@
 #   basename [-s suffix] <string> [string...]
 #
 # OVERVIEW
-#   osx style variable arguments basename
+#   variable arguments basename
 
 function basename -d "get the filename or directory part of a path"
-  if test (uname) = "Darwin"
-    command basename $argv
-  else
-    if set -q argv[1]
-      set -l ext ""
-      switch $argv[1]
-        case -s
-          if test (count $argv) -gt 2
-            set ext $argv[2]
-            set argv $argv[3..-1]
-          else
-            echo "basename: Invalid number of arguments"
-            return 1
-          end
-      end
-      for path in $argv
-        command basename "$path" "$ext"
-      end
+  if set -q argv[1]
+    set -l ext ""
+    switch $argv[1]
+      case -s
+        if test (count $argv) -gt 2
+          set ext $argv[2]
+          set argv $argv[3..-1]
+        else
+          echo "basename: Invalid number of arguments"
+          return 1
+        end
+    end
+    for path in $argv
+      command basename "$path" "$ext"
     end
   end
 end
