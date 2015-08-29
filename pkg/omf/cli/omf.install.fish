2535892c87fb92dd9d811c63bfe9c9f4c8966333
Derek Stavis <dekestavis@gmail.com>
2015-08-29 19:24:15 -0300

    omf.install: insert installed packages into bundle

--- pkg/omf/cli/omf.install.fish
+++ pkg/omf/cli/omf.install.fish
@@ -1,4 +1,26 @@
 function omf.install -a type_flag name_or_url
+  function _write_bundle
+    set bundle $OMF_CONFIG/bundle
+    set record $argv
+
+    if test -f $bundle
+      if not grep $record $bundle > /dev/null 2>&1
+        echo $record >> $bundle
+      end
+    else
+      echo $record > $bundle
+    end
+  end
+
+  function _display_success
+    echo (omf::em)"✔ $argv succesfully installed."(omf::off)
+  end
+
+  function _display_error
+    echo (omf::err)"Could not install $argv."(omf::off) 1^&2
+    return $OMF_UNKNOWN_ERR
+  end
+
   switch $type_flag
     case "--theme"
       set install_type "theme"
@@ -19,11 +41,11 @@ function omf.install -a type_flag name_or_url
       echo (omf::err)"Error: $local_name $install_type already installed."(omf::off) 1^&2
     else
       echo (omf::dim)"Trying to clone from URL..."(omf::off)
-      git clone -q $name_or_url $OMF_PATH/$parent_path/$local_name
-      and echo (omf::em)"✔ $local_name $install_type succesfully installed."(omf::off)
-      or begin
-        echo (omf::err)"$local_name is not a valid $install_type."(omf::off) 1^&2
-        return $OMF_INVALID_ARG
+      if git clone -q $name_or_url $OMF_PATH/$parent_path/$local_name
+        _write_bundle "$install_type $name_or_url"
+        _display_success "$install_type $name_or_url"
+      else
+        _display_error "$install_type $name_or_url"
       end
     end
     return 0
@@ -37,11 +59,11 @@ function omf.install -a type_flag name_or_url
     echo (omf::em)"✔ $name_or_url $install_type up to date."(omf::off)
   else
     echo (omf::dim)"Installing $name_or_url $install_type..."(omf::off)
-    git clone (cat $OMF_PATH/db/$target) $OMF_PATH/$target >/dev/null ^&1
-    and echo (omf::em)"✔ $name_or_url $install_type successfully installed."(omf::off)
-    or begin
-      echo (omf::err)"Could not install $install_type."(omf::off) 1^&2
-      return $OMF_UNKNOWN_ERR
+    if git clone (cat $OMF_PATH/db/$target) $OMF_PATH/$target >/dev/null ^&1
+      _write_bundle "$install_type $name_or_url"
+      _display_success "$install_type $name_or_url"
+    else
+      _display_error "$install_type $name_or_url"
     end
   end
 end
