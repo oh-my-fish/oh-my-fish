7dad917436f19a10aadd18590f6549ed6a7c566d
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-09-01 12:38:32 +0100

    Make `default` theme the default theme for omf.
    * On clean install, configure and download `default` theme.
    * Do not allow `default` theme to be removed.
    * When the current theme is removed, fallback to `default`.

--- pkg/omf/util/omf.util_valid_package.fish
+++ pkg/omf/util/omf.util_valid_package.fish
@@ -1,5 +1,6 @@
 function omf.util_valid_package -a package
   test (echo "$package" | tr "[:upper:]" "[:lower:]") = "omf"; and return 10
+  test (echo "$package" | tr "[:upper:]" "[:lower:]") = "default"; and return 10
   switch $package
     case {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}\*
       switch $package
