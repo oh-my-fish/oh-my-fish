033d976dee7303a23c712461cb73f694fba35b20
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-09-03 19:50:20 +0100

    Extract _write_bundle to omf.persist
    - Add support for persisting theme information.
    - Use documented private function naming scheme.

---
+++ pkg/omf/cli/omf.persist.fish
@@ -0,0 +1,23 @@
+function omf.persist -a type name_or_url
+  function __omf.write_bundle
+    set -l bundle $OMF_CONFIG/bundle
+    set -l record $argv
+
+    if test -f $bundle
+      if not grep $record $bundle > /dev/null 2>&1
+        echo $record >> $bundle
+      end
+    else
+      echo $record > $bundle
+    end
+  end
+
+  function __omf.write_theme
+    echo $argv > $OMF_CONFIG/theme
+  end
+
+  __omf.write_bundle "$type" "$name_or_url"
+  if test "$type" = theme
+    __omf.write_theme "$name_or_url"
+  end
+end
