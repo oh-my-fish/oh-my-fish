f487385ea172b74a8088a39575316d8cf4a7320c
jeremiejig <me@jeremiejig.fr>
2015-09-13 23:21:06 +0200

    setting return_code to UNKNOWN_ERROR, using better variable name

--- pkg/omf/cli/omf.install_package.fish
+++ pkg/omf/cli/omf.install_package.fish
@@ -1,10 +1,10 @@
 function omf.install_package -d "Return success for at least one successful install."
-  set -l return_code 1
+  set -l return_code $OMF_UNKNOWN_ERR
   for search in $argv
     omf.install --pkg $search
-    set -l code $status
-    if test $code -eq 0
-      set return_code $code
+    set -l install_code $status
+    if test $install_code -eq 0
+      set return_code $install_code
     end
   end
   return $return_code
