7022c3db95d6ca8587c5bfd418173edc8fb2bab9
jeremiejig <me@jeremiejig.fr>
2015-09-20 13:32:31 +0200

    tests: test commands help, query & install

--- tests/test_runner.fish
+++ tests/test_runner.fish
@@ -2,8 +2,13 @@
 
 set -l return_code 0
 
-omf help
-omf query PATH
-omf install apt
+set commands "omf help" "omf query PATH" "omf query fish_function_path" "omf install apt"
+for cmd in $commands
+  echo \$ $cmd
+  if not eval $cmd
+    set return_code 1
+  end;
+end;
+
 
 exit $return_code
