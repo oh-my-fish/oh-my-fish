59ee1820928dbffcc8100d1549e261c05e560838
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-09-27 00:15:18 +0100

    Add `require` function to support plugin dep
    In order to support dependency between plugins a function called
    `require` has been added. Each plugin needs to call this function with
    the another dependency plugin as an argument in order to pre-load the
    plugin. If the plugin does not exist, a warning message is sent to
    sdterr.

--- lib/available.fish
+++ lib/available.fish
@@ -4,6 +4,6 @@
 # OVERVIEW
 #   Check if a program is available.
 
-function available -a program -d "check if a program is available."
-  type "$program" ^/dev/null >&2
+function available -a name -d "Check if a program is available."
+  type "$name" ^/dev/null >&2
 end
