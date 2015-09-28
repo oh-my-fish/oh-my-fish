8c394771df6cc7351e4aa3390e7624abfad0e3a1
Derek Stavis <derek@dba.eng.br>
2015-09-28 16:08:21 -0300

    install: migrate script from bash to fish
    - Checks for a supported fish version (greater than 2.1)
    - Migrate travis install script to fish

--- templates/config.fish
+++ templates/config.fish
@@ -1,10 +1,8 @@
-# Path to your oh-my-fish.
-set -g OMF_PATH {{OMF_PATH}}
+# Path to Oh My Fish install.
+set -gx OMF_PATH {{OMF_PATH}}
 
-### Configuration required to load oh-my-fish ###
-# Note: Only add configurations that are required to be set before oh-my-fish is loaded.
-# For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish
-# file or to create a custom plugin instead.
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG {{OMF_CONFIG}}
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
