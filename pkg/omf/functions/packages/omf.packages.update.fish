29f7788b2f6e010232eb6ab2f081641bc63ab3bd
Itzik Ephraim <oranja@gmail.com>
2016-01-14 23:32:12 +0200

    Report up-to-date packages as such

--- pkg/omf/functions/packages/omf.packages.update.fish
+++ pkg/omf/functions/packages/omf.packages.update.fish
@@ -4,15 +4,20 @@ function omf.packages.update -a name
     not test -e $target_path/.git;
       and return 0
 
-    if omf.repo.pull $target_path
-      omf.bundle.install $target_path/bundle
-      echo (omf::em)"$name successfully updated."(omf::off)
-      return 0
-    else
-      echo (omf::err)"Could not update $name."(omf::off) 1^&2
+    omf.repo.pull $target_path
+    switch $status
+      case 0
+        omf.bundle.install $target_path/bundle
+        echo (omf::em)"$name successfully updated."(omf::off)
+        return 0
+      case 1
+        echo (omf::err)"Could not update $name."(omf::off) 1>&2
+      case 2
+        echo (omf::dim)"$name is already up-to-date."(omf::off)
+        return 0
     end
   else
-    echo (omf::err)"Could not find $name."(omf::off) 1^&2
+    echo (omf::err)"Could not find $name."(omf::off) 1>&2
   end
 
   return 1
