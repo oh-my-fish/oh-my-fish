17de8c4b6b076617bb9908daf4b0114ee1f77432
Derek Willian Stavis <dekestavis@gmail.com>
2016-05-31 18:16:02 -0300

    list: refactor code, fix fish 2.3.0 globs (#325)

--- pkg/omf/functions/cli/omf.cli.list.fish
+++ pkg/omf/functions/cli/omf.cli.list.fish
@@ -1,14 +1,12 @@
-function omf.cli.list -a type
-  test -z "$type"; and set type '--installed'
-
-  if contains -- $type '--available' '-a' '--database' '-d' '--installed' '-i'
-    omf.packages.list $type | column
-  else
-    echo (omf::err)"Invalid arguments"(omf::off)
-    echo 'Usage: omf list [ --available | -a ]'
-    echo '       omf list [ --installed | -i ]'
-    echo '       omf list [ --database  | -d ]'
-    return $OMF_INVALID_ARG
+function omf.cli.list
+  switch (count $argv)
+  case 0
+    echo (set_color -u)Plugins(set_color normal)
+    omf.packages.list --installed --plugin | column
+    echo
+    echo (set_color -u)Themes(set_color normal)
+    omf.packages.list --installed --theme | column
+  case '*'
+    omf.packages.list $argv | column
   end
-
 end
