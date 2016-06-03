c812242d955a6b30eab824431909cd93de535e67
Derek Stavis <dekestavis@gmail.com>
2016-06-02 23:16:49 -0300

    theme: reload key bindings on theme change

--- pkg/omf/functions/themes/omf.theme.set.fish
+++ pkg/omf/functions/themes/omf.theme.set.fish
@@ -30,6 +30,10 @@ function omf.theme.set -a target_theme
     end
   end
 
+  # If key bindings file found, reload fish key bindings
+  test (count {$OMF_CONFIG,$OMF_PATH}/key_binding?.fish) -gt 0
+    and __fish_reload_key_bindings
+
   # Persist the changes
   echo "$target_theme" > "$OMF_CONFIG/theme"
 
