228ef9aafe3e1d4333daf7f78e6e2602f5cc6125
Stephen M. Coakley <me@stephencoakley.com>
2016-06-15 13:20:49 -0500

    Evaluate OMF_PATH at init time to fix #136 (#348)

--- templates/config.fish
+++ templates/config.fish
@@ -1,5 +1,7 @@
 # Path to Oh My Fish install.
-set -gx OMF_PATH "{{OMF_PATH}}"
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
 #set -gx OMF_CONFIG "{{OMF_CONFIG}}"
