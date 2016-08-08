cfe8e769dbcaa31f3419b1375a0c42bb1ba80d63
Stephen M. Coakley <me@stephencoakley.com>
2016-08-08 09:00:17 -0500

    Refactor installer and config setup (#361)
    Refactor the installer to be more maintainable and interactive. The installer is now more robust and safe in its operation, and asks interactive questions for choices that the user must resolve. Setting up confuguration is also now changed to take advantage of Fish 2.3 features and does not mess with user's configuration files without permission.
    
    - Make installer smarter and use uninstaller in destroy
    - Better handling and checking for offline installs
    - `omf destroy` simply uses `install --uninstall`
    - Final warning message is no longer displayed when user intentionally aborts install
    - Backups are restored during uninstall
    - Update the README to detail the new ways to install OMF

--- pkg/omf/functions/core/omf.destroy.fish
+++ pkg/omf/functions/core/omf.destroy.fish
@@ -1,38 +1,7 @@
-function __omf.destroy.restore_backup -a file_path
-  set -l path (dirname $file_path)
-  set -l file (basename $file_path)
-  set -l name (echo $file | cut -d. -f1)
-  set -l backup_file_list $path/$name.*.copy
-  set -l backup_file_path (echo $backup_file_list | tr ' ' '\n' | sort -r | head -1)
-
-  if test -e "$backup_file_path"
-    mv "$backup_file_path" "$path/$file" ^/dev/null
-  else
-    rm -f "$path/$file" ^/dev/null
-  end
-end
-
 function omf.destroy -d "Remove Oh My Fish"
-  echo (omf::dim)"Removing Oh My Fish..."(omf::off)
+  # Run the uninstaller
+  fish "$OMF_PATH/bin/install" --uninstall "--path=$OMF_PATH" "--config=$OMF_CONFIG"
 
-  set -l installed_package_path $OMF_PATH/pkg/*
-
-  for pkg in (basename -a $installed_package_path)
-    emit uninstall_$pkg
-  end
-
-  set -q XDG_CONFIG_HOME;
-    or set -l XDG_CONFIG_HOME "$HOME/.config"
-
-  set -l fish_config_home $XDG_CONFIG_HOME/fish
-  set -l fish_prompt_home $fish_config_home/functions
-
-  __omf.destroy.restore_backup "$fish_config_home/config.fish"
-  __omf.destroy.restore_backup "$fish_prompt_home/fish_prompt.fish"
-
-  if test "$OMF_PATH" != "$HOME"
-    rm -rf "$OMF_PATH"
-  end
-
+  # Start a new OMF-free shell
   set -q CI; or exec fish < /dev/tty
 end
---
+++ tools/travis-install-git.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
+  sudo add-apt-repository -y ppa:git-core/ppa
+  sudo apt-get update
+  sudo apt-get install --only-upgrade -y git
+else
+  brew update
+  brew install git
+fi
