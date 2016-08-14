347c0916a45f78b7036a409045a72667dfc23085
Zahan Malkani <zahanm@gmail.com>
2016-08-14 02:36:47 -0700

    do not include .hg in root

--- lib/hg/hg_branch_or_bookmark_name.fish
+++ lib/hg/hg_branch_or_bookmark_name.fish
@@ -1,4 +1,4 @@
 function hg_branch_or_bookmark_name -d "Get current branch / bookmark name" -a repo_root
-  command cat $repo_root'/bookmarks.current' ^/dev/null;
-    or command cat $repo_root'/branch'
+  command cat $repo_root'/.hg/bookmarks.current' ^/dev/null;
+    or command cat $repo_root'/.hg/branch'
 end
--- lib/hg/hg_repo_root.fish
+++ lib/hg/hg_repo_root.fish
@@ -2,8 +2,10 @@ function hg_repo_root -d "Return the root of the hg repository"
   set -l dir $PWD
   while test $dir != '/'
     if test -d $dir'/.hg'
-      echo -n $dir'/.hg'
+      echo -n $dir
+      return 0
     end
-    set -l dir (dirname $dir)
+    set -l dir (dirname $dir ^/dev/null)
   end
+  return 1
 end
