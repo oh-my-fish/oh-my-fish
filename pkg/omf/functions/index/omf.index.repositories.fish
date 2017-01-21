4d4f6c227324c316075283b0ebe3779f72166096
Stephen M. Coakley <me@stephencoakley.com>
2017-01-21 16:22:49 -0500

    Test repos before adding

--- pkg/omf/functions/index/omf.index.repositories.fish
+++ pkg/omf/functions/index/omf.index.repositories.fish
@@ -20,23 +20,7 @@ function omf.index.repositories -d 'Manage package repositories'
       if set -q argv[2]
         set repo_url $argv[2]
       else
-        echo "Url not specified" >&2
-        return 1
-      end
-
-      if set -q argv[3]
-        set repo_branch $argv[3]
-      else
-        set repo_branch master
-      end
-
-      echo "$repo_url $repo_branch" >> $OMF_CONFIG/repositories
-
-    case rm remove
-      if set -q argv[2]
-        set repo_url $argv[2]
-      else
-        echo "Url not specified" >&2
+        echo "URL not specified" >&2
         return 1
       end
 
@@ -48,6 +32,44 @@ function omf.index.repositories -d 'Manage package repositories'
 
       set -l repo "$repo_url $repo_branch"
 
+      # Check if we already have the repository.
+      if test -f $OMF_CONFIG/repositories
+        if command grep -q $repo $OMF_CONFIG/repositories
+          echo "The repository is already added." >&2
+          return 1
+        end
+      end
+
+      if command grep -q $repo $OMF_PATH/repositories
+        echo "The repository is already added." >&2
+        return 1
+      end
+
+      # Before we add, do a quick ls-remote to see if the URL is a valid repo.
+      if not command git ls-remote --exit-code $repo_url refs/heads/$repo_branch > /dev/null ^&1
+        echo "The remote repository could not be found." >&2
+        return 1
+      end
+
+      echo "$repo" >> $OMF_CONFIG/repositories
+
+    case rm remove
+      if set -q argv[2]
+        set repo_url $argv[2]
+      else
+        echo "URL not specified" >&2
+        return 1
+      end
+
+      if set -q argv[3]
+        set repo_branch $argv[3]
+      else
+        set repo_branch master
+      end
+
+      set -l repo "$repo_url $repo_branch"
+
+      # Check to see if user has repositories and if the given URL is listed.
       if test -f $OMF_CONFIG/repositories
         if command grep -q $repo $OMF_CONFIG/repositories
           command grep -v $repo $OMF_CONFIG/repositories > $OMF_CONFIG/repositories.swp
@@ -57,7 +79,12 @@ function omf.index.repositories -d 'Manage package repositories'
         end
       end
 
-      echo "Could not find user repository '$repo'" >&2
+      # The given URL is not listed, check if it is a built-in repository they can't remove.
+      if command grep -q $repo $OMF_PATH/repositories
+        echo "The repository '$repo' is built-in and cannot be removed." >&2
+      else
+        echo "Could not find user repository '$repo'" >&2
+      end
       return 1
 
     case '*'
