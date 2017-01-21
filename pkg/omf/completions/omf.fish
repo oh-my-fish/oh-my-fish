44c8928797c7bdbf20d7ba5f957b198ec36656b3
Stephen M. Coakley <me@stephencoakley.com>
2017-01-21 15:59:18 -0500

    Fix completions for repo command

--- pkg/omf/completions/omf.fish
+++ pkg/omf/completions/omf.fish
@@ -3,12 +3,18 @@
 
 complete -c omf -f -d "Oh My Fish"
 
+function __omf_assert_args_count -a count
+    set -l cmd (commandline -poc)
+    set -e cmd[1]
+    test (count $cmd) -eq $count
+end
+
 set -l installed_themes (omf.packages.list --theme)
 set -l installed_plugins (omf.packages.list --plugin)
 set -l installed $installed_themes $installed_plugins
 for subcommand in "r rm remove" "c cd" "u update"
-  complete -c omf -f -n "__fish_seen_subcommand_from $subcommand" -a "$installed_themes" -d theme
-  complete -c omf -f -n "__fish_seen_subcommand_from $subcommand" -a "$installed_plugins" -d plugin
+  complete -c omf -f -n "__fish_seen_subcommand_from $subcommand; and __omf_assert_args_count 1" -a "$installed_themes" -d theme
+  complete -c omf -f -n "__fish_seen_subcommand_from $subcommand; and __omf_assert_args_count 1" -a "$installed_plugins" -d plugin
 end
 
 set -l available_themes (omf.index.query --type=theme)
@@ -21,7 +27,6 @@ end
 complete -c omf -f -n "__fish_seen_subcommand_from d desc describe" -a (printf "%s " (omf.index.query --type=plugin))
 complete -c omf -f -n "__fish_seen_subcommand_from t theme"         -a "$installed_themes"
 complete -c omf -f -n "__fish_seen_subcommand_from channel"         -a "stable dev"
-complete -c omf -f -n "__fish_seen_subcommand_from repo repositories" -a "list add remove"
 complete -c omf -f -n "__fish_seen_subcommand_from help"            -a "install theme remove update list describe cd new destroy doctor repositories"
 
 complete -c omf -f -a list     -n "__fish_use_subcommand" -d "List local packages"
@@ -31,6 +36,9 @@ complete -c omf -f -a theme    -n "__fish_use_subcommand" -d "List / Use themes"
 complete -c omf -f -a remove   -n "__fish_use_subcommand" -d "Remove a theme or package"
 complete -c omf -f -a update   -n "__fish_use_subcommand" -d "Update Oh My Fish"
 complete -c omf -f -a repositories -n "__fish_use_subcommand" -d "Manage package repositories"
+complete -c omf -f -n "__fish_seen_subcommand_from repo repositories; and __omf_assert_args_count 1" -a list -d "List installed repositories"
+complete -c omf -f -n "__fish_seen_subcommand_from repo repositories; and __omf_assert_args_count 1" -a add -d "Add a package repository"
+complete -c omf -f -n "__fish_seen_subcommand_from repo repositories; and __omf_assert_args_count 1" -a remove -d "Remove a package repository"
 complete -c omf -f -a cd       -n "__fish_use_subcommand" -d "Change directory to plugin/theme directory"
 complete -c omf -f -a new      -n "__fish_use_subcommand" -d "Create a new package from a template"
 complete -c omf -f -a search   -n "__fish_use_subcommand" -d "Search the database for a theme, package or both"
