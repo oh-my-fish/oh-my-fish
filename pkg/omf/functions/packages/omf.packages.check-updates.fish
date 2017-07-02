5126f72319755212241747de856eb1716f02b464
Itzik Ephraim <oranja@gmail.com>
2017-07-02 21:04:42 +0300

    Display fetch-phase progress with a cool progress bar.

--- pkg/omf/functions/packages/omf.packages.check-updates.fish
+++ pkg/omf/functions/packages/omf.packages.check-updates.fish
@@ -1,7 +1,9 @@
 function omf.packages.check-updates
   set -l max_workers 8
   set -l fetch_cmd "omf.repo.fetch"
-  set -l progress_indicator \u25cf
+  set -l progress_done \u25a3
+  set -l progress_left \u25a2
+  set -l progress_boxes 40
   set -l error_log (mktemp -t omf_check-updates.XXXX.log)
 
   # Supress Git's attempt to find missing repositories with different credentials.
@@ -14,7 +16,45 @@ function omf.packages.check-updates
     count (jobs | grep '$fetch_cmd')
   end
 
-  printf "Getting package updates... "
+  function __omf.packages.string_repeat -a count str
+    # `string repeat` is new in fish v2.6
+    # for older versions, use a `printf` hack
+    string repeat -Nq -n $count $str ^/dev/null
+      or printf "$str%.0s" (seq $count)
+  end
+
+  function __omf.packages.count_boxes -a left total boxes
+    set -l done (math "$total - $left")
+    set -l done_boxes (math "$done * $boxes / $total")
+    set -l left_boxes (math "$left * $boxes / $total")
+
+    # Account for rounding issues
+    if test (math "$left_boxes + $done_boxes") -lt $boxes
+      test $done_boxes -gt $left_boxes
+        and set done_boxes (math "$done_boxes + 1")
+        or set left_boxes (math "$left_boxes + 1")
+    end
+
+    echo $done_boxes
+    echo $left_boxes
+  end
+
+  function __omf.packages.display_progress -S -a left total
+    # Carriage Return to move the cursor back to the beginning of the line.
+    echo -ne "\r"
+    # Place progress boxes
+    set -l boxes (__omf.packages.count_boxes $left $total $progress_boxes)
+    if test $boxes[1] -gt 0
+      __omf.packages.string_repeat $boxes[1] $progress_done
+    end
+    if test $boxes[2] -gt 0
+      __omf.packages.string_repeat $boxes[2] $progress_left
+    end
+  end
+
+  set -l package_count (count $argv)
+
+  echo "Getting package updates... "
   while test (count $argv) -gt 0
     if test (__omf.packages.active_fetches) -lt $max_workers
       if set -l package_path (omf.packages.path $argv[1])
@@ -26,11 +66,11 @@ function omf.packages.check-updates
         # Save to log file instead.
         fish -c "$fetch_cmd $package_path" ^^$error_log &
 
-        printf "$progress_indicator"
       else
         echo (omf::err)"Could not find $argv[1]"(omf::off) >&2
       end
       set -e argv[1]
+      __omf.packages.display_progress (count $argv) $package_count
     end
     sleep 0.01s
   end
@@ -39,7 +79,9 @@ function omf.packages.check-updates
   while test (__omf.packages.active_fetches) -gt 0
     sleep 0.01s
   end
-  echo " done!"
+  echo -ne "\r done!"
+  __omf.packages.string_repeat $progress_boxes " "  # Clear the previous boxes
+  echo
 
   test (wc -l < $error_log) -gt 0
     and echo (omf::err)"Please see "(omf::off)"$error_log"(omf::err)" for errors encountered while getting updates."(omf::off)
