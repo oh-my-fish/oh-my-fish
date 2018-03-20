d1cf8bb7cc756167d068f8a7ddb53b57739ec99b
Nikolai Magnussen <nikolai@magnussen.tf>
2018-03-20 10:15:41 +0100

    Improve function discovery and pkg install check
    - The functions of a file is returned as a List - treat it as such using
    a simple for-loop to iterate over them
    - Improve install check for a package by using the omf function
    `omf.packages.path`, giving an error if the package is not recognized.

--- pkg/omf/functions/packages/omf.packages.functions.fish
+++ pkg/omf/functions/packages/omf.packages.functions.fish
@@ -1,19 +1,21 @@
 function omf.packages.functions -a pkg_name -d 'List functions provided by package'
-  set -l fun_path "$OMF_PATH/pkg/$pkg_name/functions/"
-  if not test -d $fun_path
+  set -l fun_path (omf.packages.path $pkg_name)
+  if not string length -q $fun_path
     echo "Could not find package $pkg_name"
     return 1
   end
   set -l files (find $fun_path -name "*.fish")
   echo "Functions for $pkg_name:"
   for file in $files
-    set -l fun_line (grep "^\s*function" $file)
-    set -l fun_name (echo $fun_line | sed -n 's/^\s*function \(\S\+\).*/\1/p')
-    set -l fun_desc (echo $fun_line | sed -n 's/.*function.*-d ["\']\([^"\']\+\)["\'].*/\1/p')
-    if string length -q $fun_desc
-      echo \t"$fun_name: $fun_desc"
-    else
-      echo \t"$fun_name"
+    set -l fun_lines (grep "^\s*function" $file)
+    for f in $fun_lines
+      set -l fun_name (echo $f | sed -n 's/^\s*function \(\S\+\).*/\1/p')
+      set -l fun_desc (echo $f | sed -n 's/.*function.*-d ["\']\([^"\']\+\)["\'].*/\1/p')
+      if string length -q $fun_desc
+        echo \t"$fun_name: $fun_desc"
+      else
+        echo \t"$fun_name"
+      end
     end
   end
   return 0
