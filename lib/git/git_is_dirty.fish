293414fcc247c97b887e945c1785e7232202092c
Johannes Altmanninger <aclopte@gmail.com>
2018-10-25 15:36:02 +0200

    Check whether we are inside the worktree
    Some git commands require to be run from inside the worktree (as opposed
        to the git dir, although it's usually in .git). This commit adds
    a function git_is_worktree to check this. It is used for the commands
    that need the worktree instead of git_is_repo.
    
    An alternative solution might have been to find the git worktree in the
    parent of the git directory, but this doesn't work for all cases.
    Generally it's impossible to detect the location of the worktree (plus
        it's not unique).

--- lib/git/git_is_dirty.fish
+++ lib/git/git_is_dirty.fish
@@ -1,3 +1,3 @@
 function git_is_dirty -d "Check if there are changes to tracked files"
-  git_is_repo; and not command git diff --no-ext-diff --quiet --exit-code
+  git_is_worktree; and not command git diff --no-ext-diff --quiet --exit-code
 end
--- lib/git/git_is_touched.fish
+++ lib/git/git_is_touched.fish
@@ -1,5 +1,5 @@
 function git_is_touched -d "Check if repo has any changes"
-  git_is_repo; and begin
+  git_is_worktree; and begin
     test -n (echo (command git status --porcelain))
   end
 end
