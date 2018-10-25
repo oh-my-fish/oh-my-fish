0a3258447685d4437f040174fbb8b7eed8bc62de
Johannes Altmanninger <aclopte@gmail.com>
2018-10-25 15:23:30 +0200

    Fix error  when inside a bare git repository
    For example, "git diff" would print
    "fatal: this operation must be run in a work tree"
    
    We could still run git_branch_name in bare repositories in future.

--- lib/git/git_is_repo.fish
+++ lib/git/git_is_repo.fish
@@ -1,3 +1,7 @@
 function git_is_repo -d "Check if directory is a repository"
-  test -d .git; or command git rev-parse --git-dir >/dev/null 2> /dev/null
+  test -d .git
+  or begin
+    set -l info (command git rev-parse --git-dir --is-bare-repository 2>/dev/null)
+    and test $info[2] = false
+  end
 end
