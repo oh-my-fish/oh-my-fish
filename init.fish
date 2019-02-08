168ba68ccc156b0b9cf3fa8415b429e3e5073950
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2019-02-09 00:00:39 +0100

    fixup! Remove occurrences of `?` as a glob

--- init.fish
+++ init.fish
@@ -32,10 +32,11 @@ function fish_user_key_bindings
   test -f $OMF_CONFIG/theme
     and read -l theme < $OMF_CONFIG/theme
     or set -l theme default
+  test -e $OMF_CONFIG/key_bindings.fish;
+    and source $OMF_CONFIG/key_bindings.fish
   # Prepare packages key bindings paths
-  set -l key_bindings $OMF_CONFIG/key_binding*.fish \
-                      {$OMF_CONFIG,$OMF_PATH}/pkg/*/key_bindings.fish \
-                      {$OMF_CONFIG,$OMF_PATH}/themes*/$theme/key_binding.fish
+  set -l key_bindings {$OMF_CONFIG,$OMF_PATH}/pkg/*/key_bindings.fish \
+                      {$OMF_CONFIG,$OMF_PATH}/themes*/$theme/key_bindings.fish
   # Source all keybindings collected
   for file in $key_bindings
     source $file
