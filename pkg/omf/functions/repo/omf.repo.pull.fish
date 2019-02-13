ebf53446d556e43d25c4bb4f59c2303397481f05
Yang Keao <keao.yang@yahoo.com>
2019-02-13 11:16:54 +0800

    Add branch information for update package

--- pkg/omf/functions/repo/omf.repo.pull.fish
+++ pkg/omf/functions/repo/omf.repo.pull.fish
@@ -1,11 +1,7 @@
-function omf.repo.pull
-
-  if test (count $argv) -eq 0
-    echo (omf::err)"omf.repo.pull takes a repository path as an argument."(omf::off) >&2
-    return $OMF_MISSING_ARG
+function omf.repo.pull -a repo_dir branch
+  if test -z "$branch"
+    set branch "master"
   end
-
-  set -l repo_dir $argv[1]
 
   function __omf.repo.git -V repo_dir
     command git -C "$repo_dir" $argv
@@ -21,12 +17,12 @@ function omf.repo.pull
   set initial_revision (__omf.repo.git rev-parse -q --verify HEAD);
     or return 1
 
-  # the refspec ensures that '$remote/master' gets updated
-  set -l refspec "refs/heads/master:refs/remotes/$remote/master"
+  # the refspec ensures that '$remote/$branch' gets updated
+  set -l refspec "refs/heads/$branch:refs/remotes/$remote/$branch"
   __omf.repo.git fetch --quiet $remote $refspec;
     or return 1
 
-  if test (__omf.repo.git rev-list --count master...FETCH_HEAD) -eq 0
+  if test (__omf.repo.git rev-list --count $branch...FETCH_HEAD) -eq 0
     return 2
   end
 
@@ -37,8 +33,8 @@ function omf.repo.pull
       and set stashed
   end
 
-  if test "$initial_branch" != master
-    __omf.repo.git checkout master --quiet
+  if test "$initial_branch" != $branch
+    __omf.repo.git checkout $branch --quiet
   end
 
   if not __omf.repo.git merge --ff-only --quiet FETCH_HEAD
@@ -48,7 +44,7 @@ function omf.repo.pull
     return 1
   end
 
-  if test "$initial_branch" != master
+  if test "$initial_branch" != $branch
     __omf.repo.git checkout $initial_branch --quiet
   end
 
