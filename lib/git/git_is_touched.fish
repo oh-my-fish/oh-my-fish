6361b085c3bf1fc6c5054d2aae0f6d8225e1f1ff
Fabian Homborg <FHomborg@gmail.com>
2019-06-04 17:13:48 +0200

    Fix return status
    `git diff` also returns 1 if there *is* a diff.

--- lib/git/git_is_touched.fish
+++ lib/git/git_is_touched.fish
@@ -3,6 +3,6 @@ function git_is_touched -d "Check if repo has any changes"
     # The first checks for staged changes, the second for unstaged ones.
     # We put them in this order because checking staged changes is *fast*.  
     not command git diff-index --cached --quiet HEAD -- >/dev/null 2>&1
-    or command git diff --no-ext-diff --quiet --exit-code >/dev/null 2>&1
+    or not command git diff --no-ext-diff --quiet --exit-code >/dev/null 2>&1
   end
 end
