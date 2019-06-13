82822d911b7ba582e242ebfd336b1f09cf1d23dd
Kevin Konrad <kevinkonrad@ymail.com>
2019-06-13 20:34:39 +0200

    Add PR templates and update install.sha256

--- tests/run.fish
+++ tests/run.fish
@@ -4,13 +4,23 @@
 
 set -l return_code 0
 
+omf list | grep apt 2>&1 >/dev/null
+set -l apt_installed_previously $status
+
+if test $apt_installed_previously -eq 0
+  omf remove apt
+end
+
 set commands "omf help" "omf doctor" "omf install apt"
 for cmd in $commands
   echo \$ $cmd
   if not eval $cmd
     set return_code 1
-  end;
-end;
+  end
+end
 
+if test $apt_installed_previously -ne 0
+  omf remove apt
+end
 
 exit $return_code
