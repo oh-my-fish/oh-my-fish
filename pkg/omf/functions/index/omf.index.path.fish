95bed2ce2487bbd085994885d2cec49b43dc0e16
Hernawan Faïz Abdillah <3030950+Abdillah@users.noreply.github.com>
2020-08-16 09:46:03 +0700

    Fix double slash in index path
    Double slash might disturb comparison and make them false positive.

--- pkg/omf/functions/index/omf.index.path.fish
+++ pkg/omf/functions/index/omf.index.path.fish
@@ -1,5 +1,5 @@
 function omf.index.path -d 'Get the path to the local package index'
   set -q XDG_CACHE_HOME
-    and echo "$XDG_CACHE_HOME/omf"
-    or echo "$HOME/.cache/omf"
+    and echo (string trim -r -c "/" $XDG_CACHE_HOME)"/omf"
+    or echo (string trim -r -c "/" $HOME)"/.cache/omf"
 end
