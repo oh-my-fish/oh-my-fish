be18d22daf8a0a0a8b8a69f0e71c32fe93524b43
Hernawan Faïz Abdillah <3030950+Abdillah@users.noreply.github.com>
2020-12-02 05:31:14 +0700

    Fix compatibility with older fish shell
    Suggested by @faho [here](https://github.com/oh-my-fish/oh-my-fish/pull/775#issuecomment-736224485).

--- pkg/omf/functions/index/omf.index.path.fish
+++ pkg/omf/functions/index/omf.index.path.fish
@@ -1,5 +1,5 @@
 function omf.index.path -d 'Get the path to the local package index'
   set -q XDG_CACHE_HOME
-    and echo (string trim -r -c "/" $XDG_CACHE_HOME)"/omf"
-    or echo (string trim -r -c "/" $HOME)"/.cache/omf"
+    and echo (echo $XDG_CACHE_HOME | sed 's:/*$::')"/omf"
+    or echo (echo $HOME | sed 's:/*$::')"/.cache/omf"
 end
