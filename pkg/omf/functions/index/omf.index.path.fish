f8e09ca604503956f185e7e231f61a8f8b31dc34
Hernawan Faïz Abdillah <3030950+Abdillah@users.noreply.github.com>
2020-12-02 13:19:35 +0700

    Fix double slash in index path (#775)
    * Fix double slash in index path
    
    Double slash might disturb comparison and make them false positive.
    
    * Fix compatibility with older fish shell
    
    Suggested by @faho [here](https://github.com/oh-my-fish/oh-my-fish/pull/775#issuecomment-736224485).

--- pkg/omf/functions/index/omf.index.path.fish
+++ pkg/omf/functions/index/omf.index.path.fish
@@ -1,5 +1,5 @@
 function omf.index.path -d 'Get the path to the local package index'
   set -q XDG_CACHE_HOME
-    and echo "$XDG_CACHE_HOME/omf"
-    or echo "$HOME/.cache/omf"
+    and echo (echo $XDG_CACHE_HOME | sed 's:/*$::')"/omf"
+    or echo (echo $HOME | sed 's:/*$::')"/.cache/omf"
 end
