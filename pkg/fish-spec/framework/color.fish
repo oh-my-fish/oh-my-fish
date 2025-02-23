62009f148e2cfec24b66a8b63865588c19f2fc47
Kevin F. Konrad <kevin.konrad@skillbyte.de>
2025-02-23 21:36:34 +0100

    replace implementation of fish-spec
    provide a new implementation for fish-spec with more verbose and colored
    output and more assertion functions

---
+++ pkg/fish-spec/framework/color.fish
@@ -0,0 +1,35 @@
+function echo::success
+    set_color -o green
+    echo $argv
+    set_color normal
+end
+
+function echo::failure
+    set_color -o red
+    echo $argv
+    set_color normal
+end
+
+function echo::mixed
+    set_color -o yellow
+    echo $argv
+    set_color normal
+end
+
+function echo::info
+    set_color -o cyan
+    echo $argv
+    set_color normal
+end
+
+function echo::autocolor -a total failed
+    if test $total -eq $failed -a $total -eq 0
+        echo::success $argv[3..]
+    else if test $total -eq $failed
+        echo::failure $argv[3..]
+    else if test $failed -gt 0
+        echo::mixed $argv[3..]
+    else
+        echo::success $argv[3..]
+    end
+end
--- pkg/fish-spec/functions/fish-spec.fish
+++ pkg/fish-spec/functions/fish-spec.fish
@@ -1,67 +1,95 @@
 function fish-spec
-  set -g __fish_spec_dir (dirname (dirname (status -f)))
+    # set up fish-spec
+    set -g __fish_spec_dir (dirname (dirname (status -f)))
+    for file in $__fish_spec_dir/framework/*.fish
+    source $file
+    end
 
-  # Source formatter
-  source $__fish_spec_dir/basic_formatter.fish
+    # reset global assertion counters
+    set -g __fish_spec_failed_assertions 0
+    set -g __fish_spec_total_assertions 0
 
-  # Reset internal variables
-  set -e __any_spec_failed
+    if test "$argv" = ""
+        set test_files spec/*_spec.fish
+    else
+        set test_files $argv
+    end
 
-  # Load each spec file
-  for spec_file in spec/*_spec.fish
-    source $spec_file
-  end
+    for test_file in $test_files
+        __fish_spec_run_tests_in_file $test_file
+    end
 
-  # Load helper file
-  source spec/helper.fish 2> /dev/null
-
-  emit all_specs_init
-
-  # Run all specs
-  __fish-spec.run_all_specs
-
-  emit all_specs_finished
-
-  not set -q __any_spec_failed
+    # Global summary
+    echo
+    echo::autocolor $__fish_spec_total_assertions $__fish_spec_failed_assertions "Test complete: $__fish_spec_total_assertions assertions run, $__fish_spec_failed_assertions failed."
+    if test $__fish_spec_failed_assertions -gt 0
+        return 1
+    end
 end
 
-function __fish-spec.run_all_specs
-  for suite in (functions -n | grep describe_)
-    __fish-spec.run_suite $suite
-    functions -e $suite
-  end
+function __fish_spec_run_tests_in_file -a test_file
+    # reset per file assertion counters
+    set -g __fish_spec_failed_assertions_in_file 0
+    set -g __fish_spec_total_assertions_in_file 0
+    set -g __fish_spec_last_assertion_failed no
+
+    echo::info "Running tests in $test_file..."
+    source $test_file
+
+    for suite in (functions | string match -r '^describe_.*')
+        __fish_spec_run_tests_in_suite $suite
+    end
+
+    functions -e (functions | string match -r '^(describe_.*)$')
+
+    # File-level summary
+    echo
+    echo::autocolor $__fish_spec_total_assertions_in_file $__fish_spec_failed_assertions_in_file "Summary for $test_file: $__fish_spec_total_assertions_in_file assertions, $__fish_spec_failed_assertions_in_file failed."
+    echo
 end
 
-function __fish-spec.run_suite -a suite_name
-  # This gets the list of specs that were defined on the test suite by
-  # comparing the functions names before and after the evaluation of the test suite.
-  set -l specs (begin
-    functions -n | grep it_
-    eval $suite_name >/dev/null
-    functions -n | grep it_
-  end | sort | uniq -u)
+function __fish_spec_run_tests_in_suite -a suite
+    echo::info (string replace 'describe_' 'DESCRIBE ' $suite | string replace '_' ' ')
+    $suite
 
-  functions -q before_all; and before_all
+    if functions --query before_all
+        before_all
+    end
 
-  for spec in $specs
-    emit spec_init $spec
-    functions -q before_each; and before_each
-    eval $spec
-    functions -q after_each; and after_each
-    emit spec_finished $spec
-  end
+    for test_func in (functions | string match -r '^it_.*' | sort)
+        __fish_spec_run_test_function $test_func
+    end
 
-  functions -q after_all; and after_all
+    set __fish_spec_failed_assertions (math $__fish_spec_failed_assertions + $__fish_spec_failed_assertions_in_file)
+    set __fish_spec_total_assertions (math $__fish_spec_total_assertions + $__fish_spec_total_assertions_in_file)
 
-  functions -e before_all before_each after_each after_all
+    if functions --query after_all
+        after_all
+    end
+
+    # Cleanup describe-scoped functions
+    functions -e (functions | string match -r '^(before_all|after_all|before_each|after_each|it_.*)$')
 end
 
-function __fish-spec.current_time
-  if test (uname) = 'Darwin'
-    set filename 'epoch.osx'
-  else
-    set filename 'epoch.linux'
-  end
+function __fish_spec_run_test_function -a test_func
+    set test_func_human_readable (string replace 'it_' 'IT ' $test_func | string replace -a '_' ' ')
+    echo::info $test_func_human_readable
 
-  eval $__fish_spec_dir/utils/$filename
+    if functions --query before_each
+        before_each
+    end
+
+    $test_func
+    set result $status
+
+    if functions --query after_each
+        after_each
+    end
+
+    if test $__fish_spec_last_assertion_failed = no
+        echo::success "✔ $test_func_human_readable passed!"
+    else
+        echo::failure "✘ $test_func_human_readable failed."
+        set __fish_spec_last_assertion_failed no
+    end
 end
