283943a586140c96ed8535b4d79523f89c30b5d8
Kevin F. Konrad <kevin.konrad@skillbyte.de>
2025-02-23 22:03:52 +0100

    fix indentation for fish-spec

--- pkg/fish-spec/framework/assertions.fish
+++ pkg/fish-spec/framework/assertions.fish
@@ -1,188 +1,188 @@
 function __fish_spec_assert_generic -a first second success_template failure_template test_command
-    set __fish_spec_total_assertions_in_file (math $total_assertions_in_file + 1)
+  set __fish_spec_total_assertions_in_file (math $total_assertions_in_file + 1)
 
-    if eval not "$test_command"
-        set __fish_spec_failed_assertions_in_file (math $__fish_spec_failed_assertions_in_file + 1)
-        set __fish_spec_last_assertion_failed yes
-        eval echo::failure "$failure_template"
-        return 1
-    end
-    if test "$FISH_SPEC_VERBOSE_ASSERTS" = 1
-        eval echo::success "$success_template"
-    end
+  if eval not "$test_command"
+    set __fish_spec_failed_assertions_in_file (math $__fish_spec_failed_assertions_in_file + 1)
+    set __fish_spec_last_assertion_failed yes
+    eval echo::failure "$failure_template"
+    return 1
+  end
+  if test "$FISH_SPEC_VERBOSE_ASSERTS" = 1
+    eval echo::success "$success_template"
+  end
 end
 
 function assert
-    set -l first "$argv"
-    __fish_spec_assert_generic \
-        $first unused \
-        'Assertion \"test $first\" passed!' \
-        'Assertion failed: \"test $first\" evaluated to false.' \
-        "test $argv"
+  set -l first "$argv"
+  __fish_spec_assert_generic \
+    $first unused \
+    'Assertion \"test $first\" passed!' \
+    'Assertion failed: \"test $first\" evaluated to false.' \
+    "test $argv"
 end
 
 function assert_equal -a first second
-    __fish_spec_assert_generic \
-        $first $second \
-        'Assertion \"$first\" == \"$second\" passed!' \
-        'Assertion failed: Expected \"$first\", but got \"$second\".' \
-        'test "$first" = "$second"'
+  __fish_spec_assert_generic \
+    $first $second \
+    'Assertion \"$first\" == \"$second\" passed!' \
+    'Assertion failed: Expected \"$first\", but got \"$second\".' \
+    'test "$first" = "$second"'
 end
 
 function assert_not_equal -a first second
-    __fish_spec_assert_generic \
-        $first $second \
-        'Assertion \"$first\" != \"$second\" passed!' \
-        'Assertion failed: Expected \"$second\" to be different from \"$expecte\".' \
-        'test "$first" != "$second"'
+  __fish_spec_assert_generic \
+    $first $second \
+    'Assertion \"$first\" != \"$second\" passed!' \
+    'Assertion failed: Expected \"$second\" to be different from \"$expecte\".' \
+    'test "$first" != "$second"'
 end
 
 function assert_exit_code -a expected_status
-    __fish_spec_assert_generic \
-        $expected_status $status \
-        'Assertion \"$first\" != \"$second\" passed!' \
-        'Assertion failed: Expected exit code \"$first\", but got \"$second.\"' \
-        'test "$first" -eq "$second"'
+  __fish_spec_assert_generic \
+    $expected_status $status \
+    'Assertion \"$first\" != \"$second\" passed!' \
+    'Assertion failed: Expected exit code \"$first\", but got \"$second.\"' \
+    'test "$first" -eq "$second"'
 end
 
 function assert_ok
-    assert_exit_code 0
+  assert_exit_code 0
 end
 
 function assert_true -a condition
-    __fish_spec_assert_generic \
-        $condition 0 \
-        'Assertion \"$first\" is true passed!' \
-        'Assertion failed: Expected true, but got \"$first\".' \
-        'test $first'
+  __fish_spec_assert_generic \
+    $condition 0 \
+    'Assertion \"$first\" is true passed!' \
+    'Assertion failed: Expected true, but got \"$first\".' \
+    'test $first'
 end
 
 function assert_false -a condition
-    __fish_spec_assert_generic \
-        $condition 0 \
-        'Assertion \"$first\" is false passed!' \
-        'Assertion failed: Expected false, but got \"$first\".' \
-        'test ! $first'
+  __fish_spec_assert_generic \
+    $condition 0 \
+    'Assertion \"$first\" is false passed!' \
+    'Assertion failed: Expected false, but got \"$first\".' \
+    'test ! $first'
 end
 
 function assert_match -a pattern string
-    __fish_spec_assert_generic \
-        $pattern $string \
-        'Assertion string \"$string\" matches pattern \"$pattern\" passed!' \
-        'Assertion failed: string \"$second\" does not match pattern \"$first\".' \
-        'string match -qr $first $second'
+  __fish_spec_assert_generic \
+    $pattern $string \
+    'Assertion string \"$string\" matches pattern \"$pattern\" passed!' \
+    'Assertion failed: string \"$second\" does not match pattern \"$first\".' \
+    'string match -qr $first $second'
 end
 
 
 function assert_match -a pattern string
-    __fish_spec_assert_generic \
-        $pattern $string \
-        'Assertion string \"$string\" does not match pattern \"$pattern\" passed!' \
-        'Assertion failed: string \"$second\" does not match pattern \"$first\".' \
-        'not string match -qr $first $second'
+  __fish_spec_assert_generic \
+    $pattern $string \
+    'Assertion string \"$string\" does not match pattern \"$pattern\" passed!' \
+    'Assertion failed: string \"$second\" does not match pattern \"$first\".' \
+    'not string match -qr $first $second'
 end
 
 function assert_file_exists -a file
-    __fish_spec_assert_generic \
-        $file unused \
-        'Assertion file \"$first\" exists passed!' \
-        'Assertion failed: File \"$first\" does not exist.' \
-        'test -f $first'
+  __fish_spec_assert_generic \
+    $file unused \
+    'Assertion file \"$first\" exists passed!' \
+    'Assertion failed: File \"$first\" does not exist.' \
+    'test -f $first'
 end
 
 function assert_file_does_not_exist -a file
-    __fish_spec_assert_generic \
-        $file unused \
-        'Assertion file \"$first\" does not exist passed!' \
-        'Assertion failed: File \"$first\" exists.' \
-        'not test -f $first'
+  __fish_spec_assert_generic \
+    $file unused \
+    'Assertion file \"$first\" does not exist passed!' \
+    'Assertion failed: File \"$first\" exists.' \
+    'not test -f $first'
 end
 
 function assert_directory_exists -a dir
-    __fish_spec_assert_generic \
-        $dir unused \
-        'Assertion directory \"$first\" exists passed!' \
-        'Assertion failed: Directory \"$first\" does not exist.' \
-        'test -d $dir'
+  __fish_spec_assert_generic \
+    $dir unused \
+    'Assertion directory \"$first\" exists passed!' \
+    'Assertion failed: Directory \"$first\" does not exist.' \
+    'test -d $dir'
 end
 
 function assert_directory_does_not_exist -a file
-    __fish_spec_assert_generic \
-        $file unused \
-        'Assertion directory \"$first\" does not exist passed!' \
-        'Assertion failed: Directory \"$first\" exists.' \
-        'not test -d $first'
+  __fish_spec_assert_generic \
+    $file unused \
+    'Assertion directory \"$first\" does not exist passed!' \
+    'Assertion failed: Directory \"$first\" exists.' \
+    'not test -d $first'
 end
 
 function assert_file_empty -a file
-    __fish_spec_assert_generic \
-        $file unused \
-        'Assertion file $first is empty passed!' \
-        'Assertion failed: File \"$first\" is not empty.' \
-        'not test -s $file'
+  __fish_spec_assert_generic \
+    $file unused \
+    'Assertion file $first is empty passed!' \
+    'Assertion failed: File \"$first\" is not empty.' \
+    'not test -s $file'
 end
 
 function assert_file_contains -a file content
-    __fish_spec_assert_generic \
-        $file $content \
-        'Assertion $first contains \"$second\" passed!' \
-        'Assertion failed: File \"$first\" does not contain \"$second\".' \
-        'grep -q "$second" $first'
+  __fish_spec_assert_generic \
+    $file $content \
+    'Assertion $first contains \"$second\" passed!' \
+    'Assertion failed: File \"$first\" does not contain \"$second\".' \
+    'grep -q "$second" $first'
 end
 
 function assert_file_contains_regex -a file pattern
-    __fish_spec_assert_generic \
-        $file $pattern \
-        'Assertion $first content matches regex \"$second\" passed!' \
-        'Assertion failed: File \"$first\" does not contain a string matching the pattern \"$second\".' \
-        'grep -qE "$second" $first'
+  __fish_spec_assert_generic \
+    $file $pattern \
+    'Assertion $first content matches regex \"$second\" passed!' \
+    'Assertion failed: File \"$first\" does not contain a string matching the pattern \"$second\".' \
+    'grep -qE "$second" $first'
 end
 
 function assert_not_file_contains -a file content
-    __fish_spec_assert_generic \
-        $file $content \
-        'Assertion $first does not contain \"$content\" passed!' \
-        'Assertion failed: File \"$first\" contains \"$second\".' \
-        'not grep -q "$second" $first'
+  __fish_spec_assert_generic \
+    $file $content \
+    'Assertion $first does not contain \"$content\" passed!' \
+    'Assertion failed: File \"$first\" contains \"$second\".' \
+    'not grep -q "$second" $first'
 end
 
 function assert_not_file_contains_regex -a file pattern
-    __fish_spec_assert_generic \
-        $file $pattern \
-        'Assertion $first content does not match regex \"$second\" passed!' \
-        'Assertion failed: File \"$first\" contains a string matching the pattern \"$second\".' \
-        'not grep -qE "$second" $first'
+  __fish_spec_assert_generic \
+    $file $pattern \
+    'Assertion $first content does not match regex \"$second\" passed!' \
+    'Assertion failed: File \"$first\" contains a string matching the pattern \"$second\".' \
+    'not grep -qE "$second" $first'
 end
 
 function __fish_spec_in_array_helper -a value
-    for item in $__fish_spec_assertion_array
-        if test "$item" = "$value"
-            return 0
-        end
+  for item in $__fish_spec_assertion_array
+    if test "$item" = "$value"
+      return 0
     end
-    return 1
+  end
+  return 1
 end
 
 function assert_in_array -a value
-    set -g __fish_spec_assertion_array $argv[2..]
-    __fish_spec_assert_generic \
-        $value "$__fish_spec_assertion_array" \
-        'Assertion \"$first\" in [$second] passed!' \
-        'Assertion failed: Value \"$first\" is not in the array [$second].' \
-        '__fish_spec_in_array_helper $first'
-    set result $status
-    set -e __fish_spec_assertion_array
-    return $result
+  set -g __fish_spec_assertion_array $argv[2..]
+  __fish_spec_assert_generic \
+    $value "$__fish_spec_assertion_array" \
+    'Assertion \"$first\" in [$second] passed!' \
+    'Assertion failed: Value \"$first\" is not in the array [$second].' \
+    '__fish_spec_in_array_helper $first'
+  set result $status
+  set -e __fish_spec_assertion_array
+  return $result
 end
 
 function assert_not_in_array -a value
-    set -g __fish_spec_assertion_array $argv[2..]
-    __fish_spec_assert_generic \
-        $value "$__fish_spec_assertion_array" \
-        'Assertion \"$first\" not in [$second] passed!' \
-        'Assertion failed: Value \"$first\" is in the array [$second].' \
-        'not __fish_spec_in_array_helper $first'
-    set result $status
-    set -e __fish_spec_assertion_array
-    return $result
+  set -g __fish_spec_assertion_array $argv[2..]
+  __fish_spec_assert_generic \
+    $value "$__fish_spec_assertion_array" \
+    'Assertion \"$first\" not in [$second] passed!' \
+    'Assertion failed: Value \"$first\" is in the array [$second].' \
+    'not __fish_spec_in_array_helper $first'
+  set result $status
+  set -e __fish_spec_assertion_array
+  return $result
 end
--- pkg/fish-spec/functions/fish-spec.fish
+++ pkg/fish-spec/functions/fish-spec.fish
@@ -1,95 +1,95 @@
 function fish-spec
-    # set up fish-spec
-    set -g __fish_spec_dir (dirname (dirname (status -f)))
-    for file in $__fish_spec_dir/framework/*.fish
-    source $file
-    end
+  # set up fish-spec
+  set -g __fish_spec_dir (dirname (dirname (status -f)))
+  for file in $__fish_spec_dir/framework/*.fish
+  source $file
+  end
 
-    # reset global assertion counters
-    set -g __fish_spec_failed_assertions 0
-    set -g __fish_spec_total_assertions 0
+  # reset global assertion counters
+  set -g __fish_spec_failed_assertions 0
+  set -g __fish_spec_total_assertions 0
 
-    if test "$argv" = ""
-        set test_files spec/*_spec.fish
-    else
-        set test_files $argv
-    end
+  if test "$argv" = ""
+    set test_files spec/*_spec.fish
+  else
+    set test_files $argv
+  end
 
-    for test_file in $test_files
-        __fish_spec_run_tests_in_file $test_file
-    end
+  for test_file in $test_files
+    __fish_spec_run_tests_in_file $test_file
+  end
 
-    # Global summary
-    echo
-    echo::autocolor $__fish_spec_total_assertions $__fish_spec_failed_assertions "Test complete: $__fish_spec_total_assertions assertions run, $__fish_spec_failed_assertions failed."
-    if test $__fish_spec_failed_assertions -gt 0
-        return 1
-    end
+  # Global summary
+  echo
+  echo::autocolor $__fish_spec_total_assertions $__fish_spec_failed_assertions "Test complete: $__fish_spec_total_assertions assertions run, $__fish_spec_failed_assertions failed."
+  if test $__fish_spec_failed_assertions -gt 0
+    return 1
+  end
 end
 
 function __fish_spec_run_tests_in_file -a test_file
-    # reset per file assertion counters
-    set -g __fish_spec_failed_assertions_in_file 0
-    set -g __fish_spec_total_assertions_in_file 0
-    set -g __fish_spec_last_assertion_failed no
+  # reset per file assertion counters
+  set -g __fish_spec_failed_assertions_in_file 0
+  set -g __fish_spec_total_assertions_in_file 0
+  set -g __fish_spec_last_assertion_failed no
 
-    echo::info "Running tests in $test_file..."
-    source $test_file
+  echo::info "Running tests in $test_file..."
+  source $test_file
 
-    for suite in (functions | string match -r '^describe_.*')
-        __fish_spec_run_tests_in_suite $suite
-    end
+  for suite in (functions | string match -r '^describe_.*')
+    __fish_spec_run_tests_in_suite $suite
+  end
 
-    functions -e (functions | string match -r '^(describe_.*)$')
+  functions -e (functions | string match -r '^(describe_.*)$')
 
-    # File-level summary
-    echo
-    echo::autocolor $__fish_spec_total_assertions_in_file $__fish_spec_failed_assertions_in_file "Summary for $test_file: $__fish_spec_total_assertions_in_file assertions, $__fish_spec_failed_assertions_in_file failed."
-    echo
+  # File-level summary
+  echo
+  echo::autocolor $__fish_spec_total_assertions_in_file $__fish_spec_failed_assertions_in_file "Summary for $test_file: $__fish_spec_total_assertions_in_file assertions, $__fish_spec_failed_assertions_in_file failed."
+  echo
 end
 
 function __fish_spec_run_tests_in_suite -a suite
-    echo::info (string replace 'describe_' 'DESCRIBE ' $suite | string replace '_' ' ')
-    $suite
+  echo::info (string replace 'describe_' 'DESCRIBE ' $suite | string replace '_' ' ')
+  $suite
 
-    if functions --query before_all
-        before_all
-    end
+  if functions --query before_all
+    before_all
+  end
 
-    for test_func in (functions | string match -r '^it_.*' | sort)
-        __fish_spec_run_test_function $test_func
-    end
+  for test_func in (functions | string match -r '^it_.*' | sort)
+    __fish_spec_run_test_function $test_func
+  end
 
-    set __fish_spec_failed_assertions (math $__fish_spec_failed_assertions + $__fish_spec_failed_assertions_in_file)
-    set __fish_spec_total_assertions (math $__fish_spec_total_assertions + $__fish_spec_total_assertions_in_file)
+  set __fish_spec_failed_assertions (math $__fish_spec_failed_assertions + $__fish_spec_failed_assertions_in_file)
+  set __fish_spec_total_assertions (math $__fish_spec_total_assertions + $__fish_spec_total_assertions_in_file)
 
-    if functions --query after_all
-        after_all
-    end
+  if functions --query after_all
+    after_all
+  end
 
-    # Cleanup describe-scoped functions
-    functions -e (functions | string match -r '^(before_all|after_all|before_each|after_each|it_.*)$')
+  # Cleanup describe-scoped functions
+  functions -e (functions | string match -r '^(before_all|after_all|before_each|after_each|it_.*)$')
 end
 
 function __fish_spec_run_test_function -a test_func
-    set test_func_human_readable (string replace 'it_' 'IT ' $test_func | string replace -a '_' ' ')
-    echo::info $test_func_human_readable
+  set test_func_human_readable (string replace 'it_' 'IT ' $test_func | string replace -a '_' ' ')
+  echo::info $test_func_human_readable
 
-    if functions --query before_each
-        before_each
-    end
+  if functions --query before_each
+    before_each
+  end
 
-    $test_func
-    set result $status
+  $test_func
+  set result $status
 
-    if functions --query after_each
-        after_each
-    end
+  if functions --query after_each
+    after_each
+  end
 
-    if test $__fish_spec_last_assertion_failed = no
-        echo::success "✔ $test_func_human_readable passed!"
-    else
-        echo::failure "✘ $test_func_human_readable failed."
-        set __fish_spec_last_assertion_failed no
-    end
+  if test $__fish_spec_last_assertion_failed = no
+    echo::success "✔ $test_func_human_readable passed!"
+  else
+    echo::failure "✘ $test_func_human_readable failed."
+    set __fish_spec_last_assertion_failed no
+  end
 end
