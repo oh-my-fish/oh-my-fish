b2051e16758ae4c1387c63de5bb86516861e5ced
Kevin F. Konrad <kevin.konrad@skillbyte.de>
2025-02-25 00:12:42 +0100

    ensure tests actually pass

--- pkg/fish-spec/spec/results_spec.fish
+++ pkg/fish-spec/spec/results_spec.fish
@@ -11,11 +11,21 @@ function describe_results
   end
 
   function it_fails_when_single_assertion_fails
+    set previous_assertion_counter $__fish_spec_failed_assertions_in_file
     assert 1 = 2
+    assert_exit_code 1
+    echo decrement failed assertion counter so tests pass as expected
+    set __fish_spec_failed_assertions_in_file (math $__fish_spec_failed_assertions_in_file - 1)
+    assert_equal $previous_assertion_counter $__fish_spec_failed_assertions_in_file
   end
 
   function it_fails_when_one_of_the_assertions_fails
+    set previous_assertion_counter $__fish_spec_failed_assertions_in_file
     assert 1 = 2
+    assert_exit_code 1
     assert 2 = 2
+    echo decrement failed assertion counter so tests pass as expected
+    set __fish_spec_failed_assertions_in_file (math $__fish_spec_failed_assertions_in_file - 1)
+    assert_equal $previous_assertion_counter $__fish_spec_failed_assertions_in_file
   end
 end
