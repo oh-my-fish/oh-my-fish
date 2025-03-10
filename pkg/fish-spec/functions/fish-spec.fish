f0ec166e04546e0a9196da41551a1e17a5ea9230
Kevin F. Konrad <kevin.konrad@skillbyte.de>
2025-03-10 19:57:34 +0100

    use while read instead of mktemp to capture outputs

--- pkg/fish-spec/functions/fish-spec.fish
+++ pkg/fish-spec/functions/fish-spec.fish
@@ -75,33 +75,32 @@ function __fish_spec_run_test_function -a test_func
   set test_func_human_readable (string replace 'it_' 'IT ' $test_func | string replace -a '_' ' ')
   __fish_spec.color.echo-n.info "⏳ $test_func_human_readable"
 
-  set -l before_each_output (mktemp)
+  set -l before_each_output ""
   if functions --query before_each
-    before_each 2>&1 > $before_each_output
+    before_each 2>1 | while read -l line; test -z "$before_each_output" && set before_each_output $line || set before_each_output $before_each_output\n$line; end
   end
 
-  set -l test_func_output (mktemp)
-  $test_func 2>&1 > $test_func_output
+  set -l test_func_output ""
+  $test_func 2>&1 | while read -l line; test -z "$test_func_output" && set test_func_output $line || set test_func_output $test_func_output\n$line; end
   set result $status
 
-  set -l after_each_output (mktemp)
+  set -l after_each_output ""
   if functions --query after_each
-    before_each 2>&1 > $after_each_output
+    after_each 2>&1 | while read -l line; test -z "$after_each_output" && set after_each_output $line || set after_each_output $after_each_output\n$line; end
   end
 
   if test $__fish_spec_last_assertion_failed = no
     __fish_spec.color.echo.success \r"✅ $test_func_human_readable passed!"
     if test "$FISH_SPEC_VERBOSE" = 1
-      test -s "$before_each_output" && cat "$before_each_output"
-      test -s "$test_func_output" && cat "$test_func_output"
-      test -s "$after_each_output" && cat "$after_each_output"
+      test -n "$before_each_output" && echo "$before_each_output"
+      test -n "$test_func_output" && echo "$test_func_output"
+      test -n "$after_each_output" && echo "$after_each_output"
     end
   else
     __fish_spec.color.echo.failure \r"❌ $test_func_human_readable failed."
-    test -s "$before_each_output" && cat "$before_each_output"
-    test -s "$test_func_output" && cat "$test_func_output"
-    test -s "$after_each_output" && cat "$after_each_output"
+    test -n "$before_each_output" && echo "$before_each_output"
+    test -n "$test_func_output" && echo "$test_func_output"
+    test -n "$after_each_output" && echo "$after_each_output"
     set __fish_spec_last_assertion_failed no
   end
-  rm "$before_each_output" "$test_func_output" "$after_each_output"
 end
